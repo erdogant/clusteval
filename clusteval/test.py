9551a00860438c6860891c0dfca104b79b47d562
erdogant <erdogant@gmail.com>
2020-01-23 23:38:56 +0100

    test

---
+++ clusteval/test.py
@@ -0,0 +1,14 @@
+# EXAMPLE
+import clusteval as clusteval
+from sklearn.datasets import make_blobs
+
+# %%
+[X, labels_true] = make_blobs(n_samples=750, centers=4, n_features=2, cluster_std=0.5)
+
+out = clusteval.fit(X, method='silhouette')
+out = clusteval.fit(X, method='dbindex')
+out = clusteval.fit(X, method='derivative')
+out = clusteval.fit(X, method='hdbscan')
+out = clusteval.fit(X, method='dbscan')
+
+clusteval.plot(out, X)
