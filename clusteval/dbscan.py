490f518dd6a13c3cd23d3337fea8c0f066611c62
Erdogan Taskesen <erdogant@gmail.com>
2020-06-13 23:12:19 +0200

    update docstrings dbscan

--- clusteval/dbscan.py
+++ clusteval/dbscan.py
@@ -1,103 +1,7 @@
-""" Density Based clustering.
-
-   import clusteval.dbscan as dbscan
-
-	out = dbscan.fit(X, <optional>)
-	fig = dbscan.plot(out,X, <optional>)
-
- INPUT:
-   X:              numpy datamatrix numerical
-                   rows    = features
-                   colums  = samples
- OPTIONAL
-
-  eps=             Float: The maximum distance between two samples for them to be considered as in the same neighborhood.
-                   [None] (default) Determine automatically by the Siloutte score
-                   [0.3] 
-
-  epsres=          Integer: Resoultion to test the different epsilons. The higher the longer it will take
-                   [100] (default) 
-
-  minclusters=     Integer: Minimum or more number of clusters >=
-                   [2] (default)
-
-  maxclusters=     Integer: Maximum or less number of clusters <=
-                   [25] (default)
-
-  min_samples=     Integer: [0.,,1] Percentage of expected outliers among number of samples.
-                   [0.05] (default)
-
-  metric=          string: Define your input data as type [metrics.pairwise.calculate_distance] or a distance matrix if thats the case!
-                   'euclidean' (default) squared euclidean distance
-                   'precomputed' if input is a distance matrix!
-
-  norm=            Boolean You may want to set this =0 using distance matrix as input)
-                   True: Yes (default) 
-                   False: No
-
-  n_jobs=          Integer: The number of parallel jobs to run
-                   [-1] ALL cpus (default)
-                   [1]  Use a single core
-                   
-  verbose=   Boolean [0,1]
-                   [0]: No 
-                   [1]: Some information about embedding
-                   [2]: More information about embedding (default)
-
-  showfig=         Boolean [0,1]
-                   [0]: No 
-                   [1]: Yes (default)
-
- OUTPUT
-	output
-
- DESCRIPTION
-   http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html
-   http://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html
-   
-   
- EXAMPLE
-   import clusteval.dbscan as dbscan
-
-   from sklearn.datasets.samples_generator import make_blobs
-   [X, labels_true] = make_blobs(n_samples=750, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.4,random_state=0)
-   [X, labels_true] = make_blobs(n_samples=750, centers=[[1, 1], [-1, -1], [1, -1], [-1, 1]], cluster_std=0.4,random_state=0)
-
-   out = dbscan.fit(X)
-   dbscan.plot(out, X=X)
-
-
-   from VIZ.scatter import scatter
-   scatter(X[:,0],X[:,1], size=100, labx=out['labx'])
-
-   
-   EXAMPLE 2
-   from sklearn.datasets import load_iris
-   iris = load_iris()
-   X=iris.data
-
-   from tsneBH import tsneBH
-   X   = tsneBH(iris.data)
-   out = dbscan(X)
-   scatter(X[:,0],X[:,1], size=100, labx=out['labx'])
-   scatter(X[:,0],X[:,1], size=100, labx=iris.target, labxtype='unique',title='REAL')
-
-   from UMAPet import UMAPet
-   X    = UMAPet(iris.data)
-   out = dbscan(X)
-   scatter(X[:,0],X[:,1], size=100, labx=out['labx'])
-   scatter(X[:,0],X[:,1], size=100, labx=iris.target, labx_type='unique',title='REAL')
-
-
- SEE ALSO
-   HDBSCAN, import sklearn.cluster as cluster
-"""
-
 #--------------------------------------------------------------------------
 # Name        : dbscan.py
 # Author      : E.Taskesen
 # Contact     : erdogant@gmail.com
-# Date        : Nov. 2017
 #--------------------------------------------------------------------------
 
 #%% Libraries
@@ -107,44 +11,70 @@ import matplotlib.pyplot as plt
 import sklearn.cluster as cluster
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
-from clusteval.silhouette_plot import silhouette_plot
+import clusteval.silhouette as silhouette
 
-#%% Plot
-def plot(out, X=None, width=15, height=8, verbose=3):
-    idx=out['idx']
-    [fig, ax1] = plt.subplots()
-    ax2 = ax1.twinx()
-    ax1.plot(out['eps'], out['silscores'], color='k')
-    ax1.set_xlabel('eps')
-    ax1.set_ylabel('Silhouette score')
-    ax1.grid(color='grey', linestyle='--', linewidth=0.2)
-    ax2.plot(out['eps'], out['sillclust'], color='b')
-    ax2.set_ylabel('#Clusters')
-    ax2.grid(color='grey', linestyle='--', linewidth=0.2)
-    # Plot vertical line To stress the cut-off point
-    ax2.axvline(x=out['eps'][idx], ymin=0, ymax=out['sillclust'][idx], linewidth=2, color='r')
-    
-    if not isinstance(X, type(None)):
-        if out['n_clusters']!=X.shape[0] and out['n_clusters']>1:
-            if verbose>=3: print('[DBSCAN] Estimated number of clusters: %d' %(out['n_clusters']))
-            silhouette_plot(X,out['labx'])
-    else:
-        if verbose>=3: print('[DBSCAN] data required for silhouette plot')
-        
-#%% Main function
+
+# %% Main function
 def fit(X, eps=None, min_samples=0.01, metric='euclidean', norm=True, n_jobs=-1, minclusters=2, maxclusters=25, epsres=100, verbose=3):
-	# DECLARATIONS
-    out={}
-    Param                 = {}
-    Param['verbose']      = verbose
-    Param['eps']          = eps
-    Param['metric']       = metric
-    Param['n_jobs']       = n_jobs
-    Param['norm']         = norm
-    Param['minclusters']  = minclusters
-    Param['maxclusters']  = maxclusters
-    Param['epsres']       = epsres # Resolution of the epsilon to estimate % The higher the more detailed, the more time it costs to compute. Only for DBSCAN
-    Param['min_samples']  = np.floor(min_samples*X.shape[0]) # Set max. outliers
+    """Density Based clustering.
+
+    Parameters
+    ----------
+    X : Numpy-array,
+        Where rows is features and colums are samples.
+    eps : float, (default: None)
+        The maximum distance between two samples for them to be considered as in the same neighborhood.
+        None: Determine automatically by the Siloutte score. Otherwise, 0.3 etc
+    min_samples : float [0..1], (default: 0.01)
+        Percentage of expected outliers among number of samples.
+    metric : str, (default: 'euclidean').
+        Distance measure for the clustering. Types can be found at [metrics.pairwise.calculate_distance] or a distance matrix if thats the case.
+        'euclidean' (default) squared euclidean distance or 'precomputed' if input is a distance matrix!
+    norm : bool, (default: True)
+        You may want to set this =0 using distance matrix as input.
+    n_jobs : int, (default: -1)
+        The number of parallel jobs to run. -1: ALL cpus, 1: Use a single core.
+    minclusters : int, (default: 2)
+        Minimum number of clusters >=.
+    maxclusters : int, (default: 25)
+        Maximum number of clusters <=.
+    epsres : int, (default: 100)
+        Resoultion to test the different epsilons. The higher the longer it will take.
+    verbose : int, optional (default: 3)
+        Print message to screen [1-5]. The larger the number, the more information is returned.
+
+    Returns
+    -------
+    None.
+    
+    Examples
+    --------
+    >>> import clusteval.dbscan as dbscan
+    >>> from sklearn.datasets.samples_generator import make_blobs
+    >>> [X, labels_true] = make_blobs(n_samples=750, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.4,random_state=0)
+    >>> [X, labels_true] = make_blobs(n_samples=750, centers=[[1, 1], [-1, -1], [1, -1], [-1, 1]], cluster_std=0.4,random_state=0)
+    >>> results = dbscan.fit(X)
+    >>> dbscan.plot(results, X)
+    >>> from VIZ.scatter import scatter
+    >>> scatter(X[:,0],X[:,1], size=100, labx=out['labx'])
+
+    References
+    ----------
+    * http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html
+    * http://scikit-learn.org/stable/modules/generated/sklearn.cluster.DBSCAN.html
+
+    """
+    out = {}
+    Param = {}
+    Param['verbose'] = verbose
+    Param['eps'] = eps
+    Param['metric'] = metric
+    Param['n_jobs'] = n_jobs
+    Param['norm'] = norm
+    Param['minclusters'] = minclusters
+    Param['maxclusters'] = maxclusters
+    Param['epsres'] = epsres # Resolution of the epsilon to estimate % The higher the more detailed, the more time it costs to compute. Only for DBSCAN
+    Param['min_samples'] = np.floor(min_samples*X.shape[0]) # Set max. outliers
 
     # Transform data
     if Param['norm']: 
@@ -154,7 +84,7 @@ def fit(X, eps=None, min_samples=0.01, metric='euclidean', norm=True, n_jobs=-1,
     if Param['eps']==None:
         if Param['verbose']>=3: print('[DBSCAN] Determining optimal clustering by Silhouette score..')
         # Optimize
-        [eps, sillclust, silscores, silllabx]=optimize_eps(X, eps, Param)
+        [eps, sillclust, silscores, silllabx] = _optimize_eps(X, eps, Param)
         # Store results
         idx = np.argmax(silscores)
         out['method']='dbscan'
@@ -173,8 +103,8 @@ def fit(X, eps=None, min_samples=0.01, metric='euclidean', norm=True, n_jobs=-1,
         
     return(out)
 
-#%% optimize_eps
-def optimize_eps(X, eps, Param):
+# %% optimize_eps
+def _optimize_eps(X, eps, Param):
     # Setup resolution
     eps       = np.arange(0.1,5,1/Param['epsres'])
     silscores = np.zeros(len(eps))*np.nan
@@ -210,3 +140,25 @@ def optimize_eps(X, eps, Param):
     silllabx  = silllabx[I,:]
 
     return(eps, sillclust, silscores, silllabx)
+
+# %% Plot
+def plot(out, X=None, figsize=(15,8), verbose=3):
+    idx = out['idx']
+    [fig, ax1] = plt.subplots(figsize=figsize)
+    ax2 = ax1.twinx()
+    ax1.plot(out['eps'], out['silscores'], color='k')
+    ax1.set_xlabel('eps')
+    ax1.set_ylabel('Silhouette score')
+    ax1.grid(color='grey', linestyle='--', linewidth=0.2)
+    ax2.plot(out['eps'], out['sillclust'], color='b')
+    ax2.set_ylabel('#Clusters')
+    ax2.grid(color='grey', linestyle='--', linewidth=0.2)
+    # Plot vertical line To stress the cut-off point
+    ax2.axvline(x=out['eps'][idx], ymin=0, ymax=out['sillclust'][idx], linewidth=2, color='r')
+    
+    if X is not None:
+        if out['n_clusters']!=X.shape[0] and out['n_clusters']>1:
+            if verbose>=3: print('[DBSCAN] Estimated number of clusters: %d' %(out['n_clusters']))
+            silhouette.scatter(out, X, figsize=figsize)
+    else:
+        if verbose>=3: print('[DBSCAN] data required for silhouette plot')
