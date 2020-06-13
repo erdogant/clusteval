d6dd5d5845a248839737c71235acf711eba75304
Erdogan Taskesen <erdogant@gmail.com>
2020-06-13 17:25:57 +0200

    new examples by using classes

--- clusteval/examples.py
+++ clusteval/examples.py
@@ -5,12 +5,25 @@ print(clusteval.__version__)
 print(dir(clusteval))
 
 # %%
+from clusteval import clusteval
 [X, labels_true] = make_blobs(n_samples=750, centers=4, n_features=2, cluster_std=0.5)
 
-out = clusteval.fit(X, method='silhouette')
-out = clusteval.fit(X, method='dbindex')
-out = clusteval.fit(X, method='derivative')
-out = clusteval.fit(X, method='hdbscan')
-out = clusteval.fit(X, method='dbscan')
+ce1 = clusteval(method='silhouette')
+out1 = ce1.fit(X)
+ce1.plot(X)
 
-clusteval.plot(out, X)
+ce2 = clusteval(method='dbindex')
+out2 = ce2.fit(X)
+ce2.plot(X)
+
+ce3 = clusteval(method='derivative')
+out3 = ce3.fit(X)
+ce3.plot(X)
+
+ce4 = clusteval(method='dbscan')
+out4 = ce4.fit(X)
+ce4.plot(X)
+
+ce5 = clusteval(method='hdbscan')
+out5 = ce5.fit(X)
+ce5.plot(X)
