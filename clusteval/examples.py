566f22a9336fe34697d5e67124c9d5954335caf6
Erdogan Taskesen <erdogant@gmail.com>
2020-06-13 23:13:07 +0200

    update examples

--- clusteval/examples.py
+++ clusteval/examples.py
@@ -5,15 +5,25 @@ print(clusteval.__version__)
 print(dir(clusteval))
 
 # %%
-from clusteval import clusteval
-[X, labels_true] = make_blobs(n_samples=750, centers=4, n_features=2, cluster_std=0.5)
 
-# %%
+# %% Silhouette
+from clusteval import clusteval
+X, labels_true = make_blobs(n_samples=750, centers=4, n_features=2, cluster_std=0.5)
+
 ce1 = clusteval(method='silhouette')
 ce1 = clusteval(method='silhouette', metric='kmeans', savemem=True)
 out1 = ce1.fit(X)
 ce1.plot(X)
 
+# %% DBSCAN
+[X, labels_true] = make_blobs(n_samples=750, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.4,random_state=0)
+[X, labels_true] = make_blobs(n_samples=750, centers=[[1, 1], [-1, -1], [1, -1], [-1, 1]], cluster_std=0.4,random_state=0)
+
+ce4 = clusteval(method='dbscan')
+out4 = ce4.fit(X)
+ce4.plot(X)
+
+# %%
 ce2 = clusteval(method='dbindex')
 out2 = ce2.fit(X)
 ce2.plot(X)
@@ -21,10 +31,6 @@ ce2.plot(X)
 ce3 = clusteval(method='derivative')
 out3 = ce3.fit(X)
 ce3.plot(X)
-
-ce4 = clusteval(method='dbscan')
-out4 = ce4.fit(X)
-ce4.plot(X)
 
 ce5 = clusteval(method='hdbscan')
 out5 = ce5.fit(X)
