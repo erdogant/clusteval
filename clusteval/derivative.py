3fd2da7765edb87d98c2630f84f5c7c98c334918
Erdogan Taskesen <erdogant@gmail.com>
2020-06-14 13:30:30 +0200

    code cleaning and refactoring

--- clusteval/derivative.py
+++ clusteval/derivative.py
@@ -1,60 +1,3 @@
-""" This function return the cluster labels for the optimal cutt-off based on the choosen hierarchical clustering method
-
-	out = derivative.fit(X)
-	fig = derivative.plot(out)
-
- INPUT:
-   X:            datamatrix
-                 rows    = features
-                 colums  = samples
- OPTIONAL
-
- metric=         String: Distance measure for the clustering 
-                 'euclidean' (default)
-
- linkage=        String: Linkage type for the clustering 
-                 'ward' (default)
-
- minclusters=    Integer: Minimum or more number of clusters >=
-                 [2] (default)
-
- maxclusters=    Integer: Maximum or less number of clusters <=
-                 [25] (default)
-
- showfig=        Boolean [0,1]: Progressbar
-                 [0]: No (default)
-                 [1]: Yes (silhouette plot)
-                   
- height=         Integer:  Height of figure
-                 [5]: (default)
-
- width=          Integer:  Width of figure
-                 [5]: (default)
-
- verbose=        Boolean [0,1]: Progressbar
-                 [0]: No (default)
-                 [1]: Yes
-
- OUTPUT
-	output
-
- DESCRIPTION
-  This function return the cluster labels for the optimal cutt-off based on the choosen clustering method
-  
- EXAMPLE
-   import clusteval.derivative as derivative
-
-   from sklearn.datasets import make_blobs
-   [X, labels_true] = make_blobs(n_samples=750, centers=6, n_features=10)
-   out= derivative.fit(X)
-   fig= derivative.plot(out)
-
-
- SEE ALSO
-   silhouette, dbindex, 
-"""
-#print(__doc__)
-
 #--------------------------------------------------------------------------
 # Name        : derivative.py
 # Author      : E.Taskesen
@@ -67,8 +10,56 @@ from scipy.cluster.hierarchy import fcluster
 from scipy.cluster.hierarchy import linkage as linkage_scipy
 import matplotlib.pyplot as plt
 
-#%%
-def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, width=15, Z=None, verbose=3):
+# %% Main
+def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, Z=None, verbose=3):
+    """ Determine optimal number of clusters using dbindex.
+
+    Description
+    -----------
+    This function return the cluster labels for the optimal cutt-off based on the choosen hierarchical clustering method.
+
+    Parameters
+    ----------
+    X : Numpy-array.
+        The rows are the features and the colums are the samples.
+    metric : str, (default: 'euclidean').
+        Distance measure for the clustering, such as 'euclidean','hamming', etc.
+    linkage : str, (default: 'ward')
+        Linkage type for the clustering.
+        'ward','single',',complete','average','weighted','centroid','median'.
+    minclusters : int, (default: 2)
+        Minimum number of clusters (>=).
+    maxclusters : int, (default: 25)
+        Maximum number of clusters (<=).
+    Z : Object, (default: None).
+        This will speed-up computation if you readily have Z. e.g., Z=linkage(X, method='ward', metric='euclidean').
+    verbose : int, optional (default: 3)
+        Print message to screen [1-5]. The larger the number, the more information.
+
+    Returns
+    -------
+    dict. with various keys. Note that the underneath keys can change based on the used methodtype.
+    method: str
+        Method name that is used for cluster evaluation.
+    score: None
+        Nothing in here but incuded for consistency
+    labx: list
+        Cluster labels.
+    fig: list
+        Relevant information to make the plot.
+
+    Examples
+    --------
+    >>> # Import library
+    >>> import clusteval.derivative as derivative
+    >>> from sklearn.datasets import make_blobs
+    >>> Generate demo data
+    >>> X, labels_true = make_blobs(n_samples=750, centers=6, n_features=10)
+    >>> # Fit with default parameters
+    >>> results = derivative.fit(X)
+    >>> # plot
+    >>> derivative.plot(results)
+    """
     # Make dictionary to store Parameters
     Param = {}
     Param['verbose']     = verbose
@@ -76,9 +67,9 @@ def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, wi
     Param['linkage']     = linkage
     Param['minclusters'] = minclusters
     Param['maxclusters'] = maxclusters
-    
+
     if Param['metric']=='kmeans':
-        print('Does not work with Kmeans! <return>')
+        print('[derivative] >Does not work with Kmeans! <return>')
         return
 
     # Cluster hierarchical using on metric/linkage
@@ -86,7 +77,7 @@ def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, wi
         Z = linkage_scipy(X, method=Param['linkage'], metric=Param['metric'])
 
     # Make all possible cluster-cut-offs
-    if Param['verbose']>=3: print('[DERIVATIVE] Determining optimal clustering by derivatives..')
+    if Param['verbose']>=3: print('[derivative] >Determining optimal clustering by derivatives..')
 
     # Run over all cluster cutoffs
     last = Z[-10:, 2]
@@ -97,7 +88,7 @@ def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, wi
     acceleration_rev = acceleration[::-1]
 
     k = acceleration_rev.argmax() + 2  # if idx 0 is the max of this we want 2 clusters
-    if Param['verbose']>=3: print('[DERIVATIVE] Clusters: %d' %k)
+    if Param['verbose']>=3: print('[derivative] >Clusters: %d' %k)
     
     # Now use the optimal cluster cut-off for the selection of clusters
     clustlabx = fcluster(Z, k, criterion='maxclust')
@@ -106,26 +97,42 @@ def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, wi
     clustlabx = np.array(clustlabx)
     
     # Store results
-    out = {}
-    out['method']='derivative'
-    out['labx'] = clustlabx
-    out['fig'] = {}
-    out['fig']['last_rev']=last_rev
-    out['fig']['acceleration_rev']=acceleration_rev
-    
-    return(out)
+    results = {}
+    results['method']='derivative'
+    results['labx'] = clustlabx
+    results['score'] = None
+    results['fig'] = {}
+    results['fig']['last_rev'] = last_rev
+    results['fig']['acceleration_rev'] = acceleration_rev
+    # Return
+    return(results)
 
 # %% Plot
-def plot(out, width=15, height=8):
-    idxs = np.arange(1, len(out['fig']['last_rev']) + 1)
-    k = out['fig']['acceleration_rev'].argmax() + 2  # if idx 0 is the max of this we want 2 clusters
+def plot(results, figsize=(15,8)):
+    """Make plot for the gridsearch over the number of clusters.
+
+    Parameters
+    ----------
+    results : dict.
+        Dictionary that is the output of the .fit() function.
+    figsize : tuple, (default: (15,8))
+        Figure size, (heigh,width).
+
+    Returns
+    -------
+    tuple, (fig, ax)
+        Figure and axis of the figure.
+
+    """
+    idxs = np.arange(1, len(results['fig']['last_rev']) + 1)
+    k = results['fig']['acceleration_rev'].argmax() + 2  # if idx 0 is the max of this we want 2 clusters
 
     # Make figure
-    [fig, ax1] = plt.subplots(figsize=(width, height))
+    [fig, ax1] = plt.subplots(figsize=figsize)
     # Plot
-    plt.plot(idxs, out['fig']['last_rev'])
-    plt.plot(idxs[:-2] + 1, out['fig']['acceleration_rev'])
-    
+    plt.plot(idxs, results['fig']['last_rev'])
+    plt.plot(idxs[:-2] + 1, results['fig']['acceleration_rev'])
+
     # Plot optimal cut
     ax1.axvline(x=k, ymin=0, linewidth=2, color='r',linestyle="--")
     # Set fontsizes
@@ -139,4 +146,6 @@ def plot(out, width=15, height=8):
     ax1.set_ylabel('Score')
     ax1.set_title("Derivatives versus number of clusters")
     ax1.grid(color='grey', linestyle='--', linewidth=0.2)
-    
+    plt.show()
+    # Return
+    retun(fig,ax1)
