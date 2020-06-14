3a7429f9bd82a313219834476838352afff54fa0
Erdogan Taskesen <erdogant@gmail.com>
2020-06-14 12:30:42 +0200

    plot below

--- clusteval/derivative.py
+++ clusteval/derivative.py
@@ -62,42 +62,13 @@
 # Date        : Feb. 2018
 #--------------------------------------------------------------------------
 
-#from matplotlib.pyplot import plot
 import numpy as np
 from scipy.cluster.hierarchy import fcluster
 from scipy.cluster.hierarchy import linkage as linkage_scipy
 import matplotlib.pyplot as plt
 
 #%%
-def plot(out, width=15, height=8):
-    idxs = np.arange(1, len(out['fig']['last_rev']) + 1)
-    k = out['fig']['acceleration_rev'].argmax() + 2  # if idx 0 is the max of this we want 2 clusters
-
-    # Make figure
-    [fig, ax1] = plt.subplots(figsize=(width, height))
-    # Plot
-    plt.plot(idxs, out['fig']['last_rev'])
-    plt.plot(idxs[:-2] + 1, out['fig']['acceleration_rev'])
-    
-    # Plot optimal cut
-    ax1.axvline(x=k, ymin=0, linewidth=2, color='r',linestyle="--")
-    # Set fontsizes
-    plt.rc('axes', titlesize=14)     # fontsize of the axes title
-    plt.rc('xtick', labelsize=10)     # fontsize of the axes title
-    plt.rc('ytick', labelsize=10)     # fontsize of the axes title
-    plt.rc('font', size=10)
-    # Set labels
-    ax1.set_xticks(np.arange(0,len(idxs)))
-    ax1.set_xlabel('#Clusters')
-    ax1.set_ylabel('Score')
-    ax1.set_title("Derivatives versus number of clusters")
-    ax1.grid(color='grey', linestyle='--', linewidth=0.2)
-        
-#%%
-def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, width=15, Z=[], verbose=3):
-	# DECLARATIONS
-    out ={}
-    
+def fit(X, metric='euclidean', linkage='ward', minclusters=2, maxclusters=25, width=15, Z=None, verbose=3):
     # Make dictionary to store Parameters
     Param = {}
     Param['verbose']     = verbose
