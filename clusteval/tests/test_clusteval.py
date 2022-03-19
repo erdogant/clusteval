7f56631129a06fd6872c59e5ae7ad690ad37a519
erdogant <erdogant@gmail.com>
2022-03-19 23:33:43 +0100

    update unit test

--- clusteval/tests/test_clusteval.py
+++ clusteval/tests/test_clusteval.py
@@ -2,26 +2,25 @@ from clusteval import clusteval
 from sklearn.datasets import make_blobs
 import pandas as pd
 import numpy as np
+import unittest
 
 
-# %%
-def test_fit():
-    X, y_true = make_blobs(n_samples=500, centers=6, n_features=10)
-    # ce = clusteval(evaluate='agglomerative', cluster=cluster, metric=metric, linkage=linkage, min_clust=min_clust, max_clust=max_clust, verbose=3)
-    # results = ce.fit(X)
-    # print('Clusters: %s' %(str(np.unique(results['labx']))))
+class TestCLUSTEVAL(unittest.TestCase):
 
-    # X, y_true = make_blobs(n_samples=50, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.4,random_state=0)
-    # Set all parameters to be evaluated
-    clusters = ['agglomerative', 'kmeans', 'dbscan']
-    evaluates = ['silhouette', 'dbindex', 'derivative']
-    metrics = ['euclidean', 'hamming']
-    linkages = ['ward', 'single', 'complete']
-    min_clusts = [1, 2, 10]
-    max_clusts = [1, 10, 2]
+    def test_fit(self):
+        X, y_true = make_blobs(n_samples=500, centers=6, n_features=10)
+        # X, y_true = make_blobs(n_samples=50, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.4,random_state=0)
 
-    # Evaluate across all paramters
-    out = parameter_gridtest(X, y_true, clusters, evaluates, metrics, linkages, min_clusts, max_clusts)
+        # Set all parameters to be evaluated
+        clusters = ['agglomerative', 'kmeans', 'dbscan']
+        evaluates = ['silhouette', 'dbindex', 'derivative']
+        metrics = ['euclidean', 'hamming']
+        linkages = ['ward', 'single', 'complete']
+        min_clusts = [1, 2, 10]
+        max_clusts = [1, 10, 2]
+
+        # Evaluate across all paramters
+        out = parameter_gridtest(X, y_true, clusters, evaluates, metrics, linkages, min_clusts, max_clusts)
 
 # %%
 def parameter_gridtest(X, y_true, clusters, evaluates, metrics, linkages, min_clusts, max_clusts):
@@ -62,15 +61,15 @@ def parameter_gridtest(X, y_true, clusters, evaluates, metrics, linkages, min_cl
                                     print(len(np.unique(results['labx'])))
                                     print(len(np.unique(y_true)))
                                     assert len(np.unique(results['labx']))==len(np.unique(y_true))
-                                    
+
                             except ValueError as err:
                                 assert not 'clusteval' in err.args
                                 status = err.args
                                 print(err.args)
                                 # ce.results=None
-    
+
                             # out.append(ce.results)
-                            count=count+1
+                            count=count + 1
 
     print('Fin! Total number of models evaluated with different paramters: %.0d' %(count))
     return(pd.DataFrame(out))
