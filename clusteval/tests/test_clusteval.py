410b255a8b0f895222de0bf28baf846077311200
erdogant <erdogant@gmail.com>
2025-04-24 08:49:24 +0200

    update unit tests

--- clusteval/tests/test_clusteval.py
+++ clusteval/tests/test_clusteval.py
@@ -5,53 +5,53 @@ import pandas as pd
 import numpy as np
 import unittest
 import matplotlib.pyplot as plt
+import datazets as dz
 
 
 class TestCLUSTEVAL(unittest.TestCase):
 
     def test_different_X(self):
         ce = clusteval()
-        
-        # Generate random data
-        X1, y1 = ce.import_example(data='circles')
-        X2, y2 = ce.import_example(data='moons')
-        X3, y3 = ce.import_example(data='anisotropic')
-        X4, y4 = ce.import_example(data='densities')
-        X5, y5 = ce.import_example(data='blobs', params={'random_state': 1})
-        X6, y6 = ce.import_example(data='globular')
-        X7, y7 = ce.import_example(data='uniform')
 
-        datas = [X1, X2, X3, X5, X4, X6, X7]
-        ys = [y1, y2, y3, y5, y4, y6, y7]
+        # Generate random data
+        df1 = dz.get(data='circles')
+        df2 = ce.import_example(data='moons')
+        df3 = ce.import_example(data='anisotropic')
+        df4 = ce.import_example(data='densities')
+        df5 = ce.import_example(data='blobs', params={'random_state': 1})
+        df6 = ce.import_example(data='globular')
+        df7 = ce.import_example(data='uniform')
+
+        dfs = [df1, df2, df3, df5, df4, df6, df7]
         titles = ['Noisy Circles', 'Noisy Moons', 'Anisotropic', 'Blobs', 'Different Densities', 'Globular', 'No Structure']
         fig, axs = plt.subplots(1, 7, figsize=(60, 8), dpi=100)
-        for i, data in enumerate(zip(datas, ys)):
-            axs[i].scatter(data[0][:, 0], data[0][:, 1], c=data[1])
+        for i, df in enumerate(dfs):
+            axs[i].scatter(df.loc[:, 0], df.loc[:, 1], c=df.index.values)
             axs[i].set_title(titles[i], fontsize=33)
             axs[i].set_xticks([])
             axs[i].set_yticks([])
             axs[i].set_xticklabels([])
             axs[i].set_yticklabels([])
-        
+
         methods = [['kmeans'], ['dbscan'], ['agglomerative', 'single'], ['agglomerative', 'complete'], ['agglomerative', 'ward']]
         evaluations = ['silhouette', 'dbindex', 'derivative']
         font_properties = {'size_x_axis': 18, 'size_y_axis': 18, 'size_title': 26}
         
         for k, evaluate in enumerate(evaluations):
-            fig, axs = plt.subplots(len(datas), len(methods), figsize=(60, 60), dpi=75)
+            fig, axs = plt.subplots(len(dfs), len(methods), figsize=(60, 60), dpi=75)
             fig.suptitle(evaluate.title(), fontsize=36)
-            fig2, axs2 = plt.subplots(len(datas), len(methods), figsize=(60, 60), dpi=75)
+            fig2, axs2 = plt.subplots(len(dfs), len(methods), figsize=(60, 60), dpi=75)
             fig2.suptitle(evaluate.title(), fontsize=36)
         
             # Run over data
-            for j, X in enumerate(datas):
+            for j, X in enumerate(dfs):
                 for i, method in enumerate(methods):
                     linkage = 'ward' if len(method)==1 else method[1]
                     ce = clusteval(evaluate=evaluate, cluster=method[0], metric='euclidean', linkage=linkage, max_clust=10)
                     results = ce.fit(X)
                     if (results is not None):
                         ce.plot(title='', ax=axs[j][i], showfig=False, xlabel='Nr. Clusters', ylabel='', font_properties=font_properties)
-                        axs2[j][i].scatter(X[:, 0], X[:, 1], c=results['labx'])
+                        axs2[j][i].scatter(X.loc[:, 0], X.loc[:, 1], c=results['labx'])
                         axs2[j][i].grid(True)
                         if j==0:
                             axs[j][i].set_title(' '.join(method).title(), fontsize=42)
@@ -60,15 +60,15 @@ class TestCLUSTEVAL(unittest.TestCase):
 
     def test_import_example(self):
         cl = clusteval()
-        sizes = [(1000, 4), (891, 12), (649, 33), (128, 27), (4674, 9), (14850, 40), (999, 8)]
-        datasets = ['sprinkler', 'titanic', 'student', 'fifa', 'cancer', 'waterpump', 'retail']
+        sizes = [(1000, 4), (891, 12), (649, 33), (128, 27), (4674, 9), (59400, 41), (999, 8)]
+        datasets = ['sprinkler', 'titanic', 'student', 'fifa', 'cancer', 'waterpump', 'marketing_retail']
         for data, size in zip(datasets, sizes):
             df = cl.import_example(data=data)
             assert df.shape==size
 
     def test_fit(self):
         ce = clusteval()
-        X, y_true = ce.import_example(data='blobs', params={'random_state': 1})
+        df = dz.get(data='blobs', params={'random_state': 1})
 
         # Set all parameters to be evaluated
         clusters = ['dbscan', 'agglomerative', 'kmeans']
@@ -79,7 +79,7 @@ class TestCLUSTEVAL(unittest.TestCase):
         max_clusts = [1, 10, 2]
 
         # Evaluate across all paramters
-        out = parameter_gridtest(X, y_true, clusters, evaluates, metrics, linkages, min_clusts, max_clusts)
+        out = parameter_gridtest(df, df.index.values, clusters, evaluates, metrics, linkages, min_clusts, max_clusts)
 
 # %%
 def parameter_gridtest(X, y_true, clusters, evaluates, metrics, linkages, min_clusts, max_clusts):
